be45ea0161694d7d8afe0efccdfca74a5f0b0aed
Stephen Gutekanst <stephen@hexops.com>
2021-12-24 01:08:45 -0700

    mach: update ninja generation
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- mach/ninja-generate.sh
+++ mach/ninja-generate.sh
@@ -7,6 +7,11 @@ set -ex
 # dawn_json_generator\(
 # dawn_generator\(
 # spvtools_language_header\("
+# spvtools_core_tables\("
+# spvtools_core_enums\("
+# spvtools_glsl_tables\("
+# spvtools_opencl_tables\("
+# spvtools_vendor_table
 #
 # Excluding:
 #   tint_generate_wgsl_corpus
@@ -28,4 +33,18 @@ ninja -C ./out/Debug \
     dawn_native_opengl_loader_gen \
     spvtools_language_header_debuginfo \
     spvtools_language_header_cldebuginfo100 \
-    spvtools_language_header_vkdebuginfo100
+    spvtools_language_header_vkdebuginfo100 \
+    spvtools_core_tables_unified1 \
+    spvtools_core_enums_unified1 \
+    spvtools_glsl_tables_glsl1-0 \
+    spvtools_opencl_tables_opencl1-0 \
+    spvtools_generators_inc \
+    spvtools_build_version \
+    spvtools_vendor_tables_spv-amd-shader-explicit-vertex-parameter \
+    spvtools_vendor_tables_spv-amd-shader-trinary-minmax \
+    spvtools_vendor_tables_spv-amd-gcn-shader \
+    spvtools_vendor_tables_spv-amd-shader-ballot \
+    spvtools_vendor_tables_debuginfo \
+    spvtools_vendor_tables_opencl.debuginfo.100 \
+    spvtools_vendor_tables_nonsemantic.clspvreflection \
+    spvtools_vendor_tables_nonsemantic.shader.debuginfo.100
