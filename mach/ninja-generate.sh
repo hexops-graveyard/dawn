002c4b914a8c72a10e68fec5e0b52bd177326711
Stephen Gutekanst <stephen@hexops.com>
2021-12-23 19:37:47 -0700

    mach: correct generation of spvtools_language_header
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- mach/ninja-generate.sh
+++ mach/ninja-generate.sh
@@ -6,10 +6,11 @@ set -ex
 # action\(.*gen
 # dawn_json_generator\(
 # dawn_generator\(
+# spvtools_language_header\("
 #
 # Excluding:
 #   tint_generate_wgsl_corpus
-#   webgpu_headers_gen
+#   emscripten_bits_gen
 #
 ninja -C ./out/Debug \
     remove_stale_autogen_files \
@@ -20,6 +21,11 @@ ninja -C ./out/Debug \
     dawncpp_headers_gen \
     dawncpp_gen \
     dawn_proc_gen \
+    webgpu_headers_gen \
     dawn_native_utils_gen \
+    webgpu_dawn_native_proc_gen \
     dawn_wire_gen \
-    dawn_native_opengl_loader_gen
+    dawn_native_opengl_loader_gen \
+    spvtools_language_header_debuginfo \
+    spvtools_language_header_cldebuginfo100 \
+    spvtools_language_header_vkdebuginfo100
