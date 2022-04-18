1250f9326ee8bce9fdb3f7bb6ccd86a96e64fbaa
Stephen Gutekanst <stephen@hexops.com>
2022-04-17 22:23:12 -0700

    mach: update ninja code generation targets
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>

--- mach/ninja-generate.sh
+++ mach/ninja-generate.sh
@@ -2,19 +2,15 @@
 set -ex
 
 # Determined via editor regexp searches for the following:
-# (make sure your editor is searching all files / not respecting gitignore)
+# (make sure your editor is searching all files / not respecting .gitignore)
+# excluding files: *fuzzer*,*catapult*,*webgpu-cts*
 #
 # action\(".*gen
 # dawn_json_generator\(
 # dawn_generator\(
-# spvtools_language_header\("
-# spvtools_core_tables\("
-# spvtools_core_enums\("
-# spvtools_glsl_tables\("
-# spvtools_opencl_tables\("
-# spvtools_vendor_tables = 
 #
 # Excluding:
+#   spvtools_generators_inc
 #   generate_v8_inspector_fuzzer_corpus
 #   gen_javascript_parser_proto
 #   generate_about_tracing
@@ -23,7 +19,6 @@ set -ex
 #
 ninja -C ./out/Debug \
     remove_stale_autogen_files \
-    spvtools_generators_inc \
     vulkan_gen_icd_json_file \
     vulkan_gen_json_files \
     headers_gen \
@@ -35,21 +30,5 @@ ninja -C ./out/Debug \
     webgpu_dawn_native_proc_gen \
     mock_webgpu_gen \
     gen \
-    opengl_loader_gen \
-    spvtools_language_header_debuginfo \
-    spvtools_language_header_cldebuginfo100 \
-    spvtools_language_header_vkdebuginfo100 \
-    spvtools_core_tables_unified1 \
-    spvtools_core_enums_unified1 \
-    spvtools_glsl_tables_glsl1-0 \
-    spvtools_opencl_tables_opencl1-0 \
-    spvtools_vendor_tables_spv-amd-shader-explicit-vertex-parameter \
-    spvtools_vendor_tables_spv-amd-shader-trinary-minmax \
-    spvtools_vendor_tables_spv-amd-gcn-shader \
-    spvtools_vendor_tables_spv-amd-shader-ballot \
-    spvtools_vendor_tables_debuginfo \
-    spvtools_vendor_tables_opencl.debuginfo.100 \
-    spvtools_vendor_tables_nonsemantic.clspvreflection \
-    spvtools_vendor_tables_nonsemantic.shader.debuginfo.100 \
-    spvtools_generators_inc \
-    spvtools_build_version \
+    dawn_version_gen \
+    opengl_loader_gen
