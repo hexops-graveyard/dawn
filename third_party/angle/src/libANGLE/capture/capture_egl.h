f652a5128d47524a8075519456cb7a34e5eff958
Stephen Gutekanst <stephen@hexops.com>
2022-07-10 16:23:05 -0700

    generated: commit vendored dependencies
    Signed-off-by: Stephen Gutekanst <stephen@hexops.com>
    Change-Id: If3594c8e4a8a0e0715cf92d8eb7d715b001208fa

---
+++ third_party/angle/src/libANGLE/capture/capture_egl.h
@@ -0,0 +1,80 @@
+//
+// Copyright 2022 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// capture_egl.h:
+//   EGL capture functions
+//
+
+#ifndef LIBANGLE_CAPTURE_EGL_H_
+#define LIBANGLE_CAPTURE_EGL_H_
+
+#include "libANGLE/Context.h"
+#include "libANGLE/Thread.h"
+#include "libANGLE/capture/FrameCapture.h"
+
+namespace gl
+{
+enum class GLenumGroup;
+}
+
+namespace egl
+{
+
+template <typename CaptureFuncT, typename... ArgsT>
+void CaptureCallToCaptureEGL(CaptureFuncT captureFunc, egl::Thread *thread, ArgsT... captureParams)
+{
+    gl::Context *context = thread->getContext();
+    if (!context)
+    {
+        return;
+    }
+
+    angle::FrameCaptureShared *frameCaptureShared =
+        context->getShareGroup()->getFrameCaptureShared();
+    if (!frameCaptureShared->isCapturing())
+    {
+        return;
+    }
+
+    angle::CallCapture call = captureFunc(context, captureParams...);
+
+    frameCaptureShared->captureCall(context, std::move(call), true);
+}
+
+angle::CallCapture CaptureCreateNativeClientBufferANDROID(gl::Context *context,
+                                                          const egl::AttributeMap &attribMap,
+                                                          EGLClientBuffer eglClientBuffer);
+angle::CallCapture CaptureEGLCreateImage(gl::Context *context,
+                                         EGLenum target,
+                                         EGLClientBuffer buffer,
+                                         const egl::AttributeMap &attributes,
+                                         egl::Image *image);
+angle::CallCapture CaptureEGLDestroyImage(gl::Context *context,
+                                          egl::Display *display,
+                                          egl::Image *image);
+
+angle::CallCapture CaptureEGLCreatePbufferSurface(gl::Context *context,
+                                                  const AttributeMap &attrib_list,
+                                                  egl::Surface *surface);
+
+angle::CallCapture CaptureEGLDestroySurface(gl::Context *context,
+                                            Display *display,
+                                            Surface *surface);
+
+angle::CallCapture CaptureEGLBindTexImage(gl::Context *context,
+                                          egl::Surface *surface,
+                                          EGLint buffer);
+
+angle::CallCapture CaptureEGLReleaseTexImage(gl::Context *context,
+                                             egl::Surface *surface,
+                                             EGLint buffer);
+
+angle::CallCapture CaptureEGLMakeCurrent(gl::Context *dummy,
+                                         Surface *drawSurface,
+                                         Surface *readSurface,
+                                         gl::Context *context);
+}  // namespace egl
+
+#endif  // LIBANGLE_FRAME_CAPTURE_H_
