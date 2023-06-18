9f851886be90829ea064bf2752df820bb91b3f05
Stephen Gutekanst <stephen@hexops.com>
2023-06-18 12:58:38 -0700

    Revert "Add DAWN_USE_WINDOWS_UI option for Zig & MinGW compilers"
    This reverts commit 48e5dc5d49998aa6ca0e3607491dd11f6402a956.

--- src/dawn/native/Surface.cpp
+++ src/dawn/native/Surface.cpp
@@ -19,10 +19,10 @@
 #include "dawn/native/Instance.h"
 #include "dawn/native/SwapChain.h"
 
-#if defined(DAWN_USE_WINDOWS_UI)
+#if DAWN_PLATFORM_IS(WINDOWS)
 #include <windows.ui.core.h>
 #include <windows.ui.xaml.controls.h>
-#endif  // defined(DAWN_USE_WINDOWS_UI)
+#endif  // DAWN_PLATFORM_IS(WINDOWS)
 
 #if defined(DAWN_USE_X11)
 #include "dawn/common/xlib_with_undefs.h"
@@ -99,6 +99,7 @@ MaybeError ValidateSurfaceDescriptor(const InstanceBase* instance,
     }
 #endif  // DAWN_PLATFORM_IS(ANDROID)
 
+#if DAWN_PLATFORM_IS(WINDOWS)
 #if DAWN_PLATFORM_IS(WIN32)
     const SurfaceDescriptorFromWindowsHWND* hwndDesc = nullptr;
     FindInChain(descriptor->nextInChain, &hwndDesc);
@@ -107,7 +108,6 @@ MaybeError ValidateSurfaceDescriptor(const InstanceBase* instance,
         return {};
     }
 #endif  // DAWN_PLATFORM_IS(WIN32)
-#if defined(DAWN_USE_WINDOWS_UI)
     const SurfaceDescriptorFromWindowsCoreWindow* coreWindowDesc = nullptr;
     FindInChain(descriptor->nextInChain, &coreWindowDesc);
     if (coreWindowDesc) {
@@ -130,7 +130,7 @@ MaybeError ValidateSurfaceDescriptor(const InstanceBase* instance,
                         "Invalid SwapChainPanel");
         return {};
     }
-#endif  // defined(DAWN_USE_WINDOWS_UI)
+#endif  // DAWN_PLATFORM_IS(WINDOWS)
 
 #if defined(DAWN_USE_WAYLAND)
     const SurfaceDescriptorFromWaylandSurface* waylandDesc = nullptr;
@@ -205,15 +205,15 @@ Surface::Surface(InstanceBase* instance, const SurfaceDescriptor* descriptor)
         mHInstance = hwndDesc->hinstance;
         mHWND = hwndDesc->hwnd;
     } else if (coreWindowDesc) {
-#if defined(DAWN_USE_WINDOWS_UI)
+#if DAWN_PLATFORM_IS(WINDOWS)
         mType = Type::WindowsCoreWindow;
         mCoreWindow = static_cast<IUnknown*>(coreWindowDesc->coreWindow);
-#endif  // defined(DAWN_USE_WINDOWS_UI)
+#endif  // DAWN_PLATFORM_IS(WINDOWS)
     } else if (swapChainPanelDesc) {
-#if defined(DAWN_USE_WINDOWS_UI)
+#if DAWN_PLATFORM_IS(WINDOWS)
         mType = Type::WindowsSwapChainPanel;
         mSwapChainPanel = static_cast<IUnknown*>(swapChainPanelDesc->swapChainPanel);
-#endif  // defined(DAWN_USE_WINDOWS_UI)
+#endif  // DAWN_PLATFORM_IS(WINDOWS)
     } else if (xDesc) {
         mType = Type::XlibWindow;
         mXDisplay = xDesc->display;
@@ -285,7 +285,7 @@ void* Surface::GetHWND() const {
 IUnknown* Surface::GetCoreWindow() const {
     ASSERT(!IsError());
     ASSERT(mType == Type::WindowsCoreWindow);
-#if defined(DAWN_USE_WINDOWS_UI)
+#if DAWN_PLATFORM_IS(WINDOWS)
     return mCoreWindow.Get();
 #else
     return nullptr;
@@ -295,7 +295,7 @@ IUnknown* Surface::GetCoreWindow() const {
 IUnknown* Surface::GetSwapChainPanel() const {
     ASSERT(!IsError());
     ASSERT(mType == Type::WindowsSwapChainPanel);
-#if defined(DAWN_USE_WINDOWS_UI)
+#if DAWN_PLATFORM_IS(WINDOWS)
     return mSwapChainPanel.Get();
 #else
     return nullptr;
